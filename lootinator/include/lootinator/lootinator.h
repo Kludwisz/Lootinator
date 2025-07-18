799a31ce7844addf5146795ea6b665671a23034a
Chen Steenvoorden <dutchen18@gmail.com>
2025-07-18 14:55:47 +0200

    Add CMake build config and basic project structure

---
+++ lootinator/include/lootinator/lootinator.h
@@ -0,0 +1,8 @@
+#ifndef LOOTINATOR_LOOTINATOR_H
+#define LOOTINATOR_LOOTINATOR_H
+
+namespace loot {
+	void hello();
+}
+
+#endif
---
+++ lootinator/src/lootinator.cpp
@@ -0,0 +1,9 @@
+#include "lootinator/lootinator.h"
+
+#include <iostream>
+
+namespace loot {
+	void hello() {
+		std::cout << "Hello, Lootinator!" << std::endl;
+	}
+}
