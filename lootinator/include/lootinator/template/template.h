4f6c3fe4632708cd8c3059b61ea62ef7ff661eec
Kludwisz <kludwisz@gmail.com>
2025-07-21 11:56:24 +0200

    Define template generation interface

--- lootinator/include/lootinator/template/template.h
+++ lootinator/include/lootinator/template/template.h
@@ -18,14 +18,20 @@ namespace loot {
     class Template {
     protected:
         TemplateParameters params;
-        // here is the right spot for helper functions shared by all subclasses of Template:
+        // here is the right spot for helper functions shared by subclasses of Template:
         // - add array of constants to memory: either CPU, global, global-shared, managed, or constant 
         // - save loot seed result
         // ...
 
     public:
-        Template(const TemplateParameters& params); // user input
-        virtual std::string generate() const; // user output
+        Template(const TemplateParameters& params);
+
+        // has to be defined by all subclasses of Template, generates and outputs
+        // the entire piece of code to the out stream.
+        virtual void generate(std::ostream& out) const = 0;
+
+        // returns the result of generate(std::ostream&) as a string.
+        std::string generate() const; 
     };
 }
 
--- lootinator/src/templates/template.cpp
+++ lootinator/src/templates/template.cpp
@@ -1,7 +1,17 @@
 #include "lootinator/template/template.h"
 
+#include <sstream>
+
+
 namespace loot {
     Template::Template(const TemplateParameters &params) {
         this->params = params;
     }
+
+    std::string Template::generate() const {
+        std::ostringstream oss;
+        generate(oss);
+        return oss.str();
+    }
 }
+
